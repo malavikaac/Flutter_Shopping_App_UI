4252b8d2f0d18d0e48172f06e7d19010aae7f9cb
malavikaac <acmalavika@gmail.com>
2023-08-12 15:36:41 +0100

    chore

--- lib/bottom navigation pages/explrorepage/products_page.dart
+++ lib/bottom navigation pages/explrorepage/products_page.dart
@@ -14,32 +14,26 @@ class _ProductPageState extends State<ProductPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(actions: [
-        Column(
+        Row(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                // IconButton(
-                //     onPressed: () {
-                //       Navigator.push(
-                //           context,
-                //           MaterialPageRoute(
-                //               builder: (context) => const HomePage()));
-                //     },
-                //     icon: const Icon(Icons.arrow_back_ios_new)),
+            // IconButton(
+            //     onPressed: () {
+            //       Navigator.push(
+            //           context,
+            //           MaterialPageRoute(
+            //               builder: (context) => const HomePage()));
+            //     },
+            //     icon: const Icon(Icons.arrow_back_ios_new)),
 
-                myWidget.titleText(
-                  text: "B&o",
-                  fontSize: 25,
-                  fontWeight: FontWeight.w400,
-                ), //titleText
-                myWidget.iconAvatar(
-                    backGroundColor: myColors.greenColor,
-                    icon: Icons.search,
-                    iconColor: myColors.whiteColor)
-              ],
-            ),
+            myWidget.titleText(
+              text: "B&o",
+              fontSize: 25,
+              fontWeight: FontWeight.w400,
+            ), //titleText
+            myWidget.iconAvatar(
+                backGroundColor: myColors.greenColor,
+                icon: Icons.search,
+                iconColor: myColors.whiteColor)
           ],
         ),
       ]),
