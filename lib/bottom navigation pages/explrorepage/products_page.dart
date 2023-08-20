cc45bb077fc4e7d1e927e000c761eb573024af28
malavikaac <acmalavika@gmail.com>
2023-08-20 17:52:37 +0100

    chore

--- lib/bottom navigation pages/explrorepage/products_page.dart
+++ lib/bottom navigation pages/explrorepage/products_page.dart
@@ -13,30 +13,42 @@ class _ProductPageState extends State<ProductPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(actions: [
-        Row(
-          children: [
-            // IconButton(
-            //     onPressed: () {
-            //       Navigator.push(
-            //           context,
-            //           MaterialPageRoute(
-            //               builder: (context) => const HomePage()));
-            //     },
-            //     icon: const Icon(Icons.arrow_back_ios_new)),
+      appBar: AppBar(
+        actions: [
+          Row(
+            children: [
+              // IconButton(
+              //     onPressed: () {
+              //       Navigator.push(
+              //           context,
+              //           MaterialPageRoute(
+              //               builder: (context) => const HomePage()));
+              //     },
+              //     icon: const Icon(Icons.arrow_back_ios_new)),
 
-            myWidget.titleText(
-              text: "B&o",
-              fontSize: 25,
-              fontWeight: FontWeight.w400,
-            ), //titleText
-            myWidget.iconAvatar(
-                backGroundColor: myColors.greenColor,
-                icon: Icons.search,
-                iconColor: myColors.whiteColor)
-          ],
-        ),
-      ]),
+              myWidget.titleText(
+                text: "B&o",
+                fontSize: 25,
+                fontWeight: FontWeight.w400,
+              ), //titleText
+              myWidget.iconAvatar(
+                  backGroundColor: myColors.greenColor,
+                  icon: Icons.search,
+                  iconColor: myColors.whiteColor),
+            ],
+          ),
+        ],
+        // bottom: TabBar(
+        //   tabs: [
+        //     Tab(text: 'All'),
+        //     Tab(
+        //       text: 'HeadSet',
+        //     ),
+        //     Tab(text: 'Sheos'),
+        //     Tab(text: 'Lights'),
+        //   ],
+        // ),
+      ),
     );
   }
 }
