82a8c9595a552ddb96035c5b56ed4ac02a0f1d51
malavikaac <acmalavika@gmail.com>
2023-09-02 17:45:51 +0100

    feat - tab bar

--- lib/bottom navigation pages/explrorepage/products_page.dart
+++ lib/bottom navigation pages/explrorepage/products_page.dart
@@ -10,28 +10,51 @@ class ProductPage extends StatefulWidget {
 }
 
 class _ProductPageState extends State<ProductPage> {
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        actions: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              myWidget.titleText(
-                text: "Products",
-                fontSize: 20,
-                fontWeight: FontWeight.w400,
-                color:Colors.black
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          centerTitle: true,
+          title: Text("Products",
+          style: TextStyle(
+            color:myColors.blackColor
+          ),
+          ) ,                                                           
+         iconTheme: IconThemeData(color:myColors.blackColor),
+          backgroundColor: myColors.whiteColor,
+          actions: [  
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+              child: myWidget.iconAvatar(
+                  backGroundColor: myColors.greenColor,
+                  icon: Icons.search,
+                  iconColor: myColors.whiteColor),
+            ),
+          ],
+          bottom:  TabBar(
+            unselectedLabelColor: myColors.greyTextColor,
+             
+            tabs: const [
+              Tab(
+                text: "All",
               ),
+              Tab(
+                text: "Headset",
+              ),
+              Tab(
+                text: "Speakers",
+              ),
+              Tab(
+                text: "Mic",
+              )
             ],
-          ), //titleText
-          myWidget.iconAvatar(
-              backGroundColor: myColors.greenColor,
-              icon: Icons.search,
-              iconColor: myColors.whiteColor),
-        ],
+          ),
+        
+        ),
       ),
     );
   }
