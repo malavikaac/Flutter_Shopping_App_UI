60bf4fc1b03a7134b618f57465b2ac1025c744b1
malavikaac <acmalavika@gmail.com>
2023-09-19 17:23:00 +0100

    feat - added a images in all products Page

--- lib/bottom navigation pages/explrorepage/all_products_page.dart
+++ lib/bottom navigation pages/explrorepage/all_products_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_ui_screen/constants/colors.dart';
+import 'package:shopping_ui_screen/constants/widgets.dart';
 
 class AllProdudtsPage extends StatefulWidget {
   const AllProdudtsPage({super.key});
@@ -11,10 +12,123 @@ class AllProdudtsPage extends StatefulWidget {
 class _AllProdudtsPageState extends State<AllProdudtsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      width: 200,
-      color: myColors.iconBottomGreyColor,
+    return  Padding(
+      padding: const EdgeInsets.symmetric(horizontal:25,vertical: 15),
+      child: Column(
+        children: [
+          Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                    height: 200,
+                                    child: Image.asset("assets/images/lamp.png")),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                myWidget.titleText(
+                                    fontWeight: FontWeight.w500,
+                                    text: " B&o Desk Lamp",
+                                    fontSize: 16), //product name
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                myWidget.titleText(
+                                    fontWeight: FontWeight.w600,
+                                    text: "\$ 450",
+                                    fontSize: 16,
+                                    color: myColors.greenColor), //product price
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                    height: 200,
+                                    child: Image.asset("assets/images/watch.png")),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                myWidget.titleText(
+                                    fontWeight: FontWeight.w500,
+                                    text: "Leather Wristwatch",
+                                    fontSize: 16), //product name
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                myWidget.titleText(
+                                    fontWeight: FontWeight.w600,
+                                    text: "\$ 254",
+                                    fontSize: 16,
+                                    color: myColors.greenColor), //product price
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height:20,),
+                       Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                              height: 200,
+                              child: Image.asset("assets/images/lamp.png")),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          myWidget.titleText(
+                              fontWeight: FontWeight.w500,
+                              text: " B&o Desk Lamp",
+                              fontSize: 16), //product name
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          myWidget.titleText(
+                              fontWeight: FontWeight.w600,
+                              text: "\$ 450",
+                              fontSize: 16,
+                              color: myColors.greenColor), //product price
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                              height: 200,
+                              child: Image.asset("assets/images/watch.png")),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          myWidget.titleText(
+                              fontWeight: FontWeight.w500,
+                              text: "Leather Wristwatch",
+                              fontSize: 16), //product name
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          myWidget.titleText(
+                              fontWeight: FontWeight.w600,
+                              text: "\$ 254",
+                              fontSize: 16,
+                              color: myColors.greenColor), //product price
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+        ],
+      ),
     );
   }
 }
