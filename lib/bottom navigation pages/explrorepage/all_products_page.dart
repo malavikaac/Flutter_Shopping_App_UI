a53f79f28a8f622e0d4e3d11604a9d3293479424
malavikaac <acmalavika@gmail.com>
2023-10-03 17:32:33 +0100

    chore

--- lib/bottom navigation pages/explrorepage/all_products_page.dart
+++ lib/bottom navigation pages/explrorepage/all_products_page.dart
@@ -127,6 +127,62 @@ class _AllProdudtsPageState extends State<AllProdudtsPage> {
                     ),
                   ],
                 ),
+                      const SizedBox(height:20,),
+                       Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                              height: 200,
+                              child: Image.asset("assets/images/heels.png")),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          myWidget.titleText(
+                              fontWeight: FontWeight.w500,
+                              text: "Heels",
+                              fontSize: 16), //product name
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          myWidget.titleText(
+                              fontWeight: FontWeight.w600,
+                              text: "\$ 60",
+                              fontSize: 16,
+                              color: myColors.greenColor), //product price
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                              height: 200,
+                              child: Image.asset("assets/images/phone.png")),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          myWidget.titleText(
+                              fontWeight: FontWeight.w500,
+                              text: "Phones",
+                              fontSize: 16), //product name
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          myWidget.titleText(
+                              fontWeight: FontWeight.w600,
+                              text: "\$ 494",
+                              fontSize: 16,
+                              color: myColors.greenColor), //product price
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
         ],
       ),
     );
